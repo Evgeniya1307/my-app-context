42f4692dd381082b52203ae799a28b19b95d1438
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-21 14:06:05 +0400

    onClick={() => setActiveTab("html")}           className={`${styles.item} ${activeTab === "html" ? styles.activeTab : ""}`}// делаю проверку если активный таб ===строке html ? тогда клас у нас будет styles.activeTab а в противном случае : ""

--- src/components/CodeBar/CodeBar.js
+++ src/components/CodeBar/CodeBar.js
@@ -1,27 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./CodeBar.module.css";
 
+const CodeBar = () => {
+  // определяюсь какой tab показывать html css js делаю это при момощи состояния
+  const [activeTab, setActiveTab] = useState("html");
 
-const CodeBar = ()=> {
-    return(
-        <div className={styles.codebar}>
-        <nav className={styles.tab}>
-          <button
-          className={`${styles.item} ${styles.activeTab}`}>
-              HTML
-          </button> 
-          <button
-          className={styles.item}>
-             CSS
-          </button> 
-          <button
-          className={styles.item}>
-   JS
-          </button> 
-        </nav>
-
-        </div>
-    )
-}
+  return (
+    <div className={styles.codebar}>
+      <nav className={styles.tab}>
+        <button
+          onClick={() => setActiveTab("html")}
+          className={`${styles.item} ${activeTab === "html" ? styles.activeTab : ""}`}// делаю проверку если активный таб ===строке html ? тогда клас у нас будет styles.activeTab а в противном случае : ""
+        >
+          HTML
+        </button>
+        <button onClick={() => setActiveTab("css")} className={styles.item}>
+          CSS
+        </button>
+        <button onClick={() => setActiveTab("js")} className={styles.item}>
+          JS
+        </button>
+      </nav>
+    </div>
+  );
+};
 
 export default CodeBar;
