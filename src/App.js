291d078181d93401dcbbf493cea58b06430fa12e
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-21 13:49:50 +0400

    <CodeBar/>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './App.css';
+import CodeBar from "./components/CodeBar/CodeBar";
 import { EditorProvider } from "./context/context";
 
 function App() {
   return (
     //всё приложение обернуть в провайдер который мы сделали в context
 <EditorProvider>
-
+<CodeBar/>
 </EditorProvider>
 
       
