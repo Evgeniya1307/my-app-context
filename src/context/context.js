ea2d8ad2ddd9c4de59f87ebaaefd078a4f9a812b
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-21 13:25:37 +0400

    import React, {createContext, useState} from "react";

---
+++ src/context/context.js
@@ -0,0 +1,10 @@
+import React, {createContext, useState} from "react";
+
+
+const EditorContext =  createContext()
+// делаю провайдер
+const EditorProvider = ({children}) => {
+
+   //делаю состояния для html и js
+
+}
