a1855d61a728942a8be611086daa8ef0b2153d61
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-21 15:44:38 +0400

    function JsEditor() {   //вытаскиваюданные с js    const{js, setJs}= useContext(EditorContext)

--- src/components/CodeBar/Editor/JsEditor.js
+++ src/components/CodeBar/Editor/JsEditor.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, {useContext}from "react";
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/theme-monokai";
 import "ace-builds/src-noconflict/snippets/javascript";
 import "ace-builds/src-noconflict/ext-language_tools";
+import { EditorContext } from '../../../context/context';
 
 function JsEditor() {
+  //вытаскиваюданные с js 
+  const{js, setJs}= useContext(EditorContext)
   return (
     <AceEditor
       placeholder="Write your JS codes here!"
       mode="javascript"
       theme="monokai"
       name="editor_js"
+      value={js}
+      onChange={value=>setJs(value)}//сюда записываем что вводим в поле
       fontSize="16"
       heigth={"100%"}
       width={"100%"}
